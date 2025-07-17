13967d8c5b91375584ccb78de865c171e8b0a9d6
Anna <anna.briancon33@gmail.com>
2025-07-17 17:06:26 +0200

    fix

--- back/src/models/index.js
+++ back/src/models/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const sequelize = require('../config/database');
+const sequelize = require('../utils/sequelize');
 
 const models = {};
 
